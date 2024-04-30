c869bf6587ee491acc314958908fc5e0211567c8
SUPRANSHU SINGH <ss445198@gmail.com>
2024-04-30 22:51:46 +0530

    Signup Binding Done

--- WritingDeskFront/src/app/pages/signup/signup.component.ts
+++ WritingDeskFront/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Agent } from 'http';
+import { ApisService } from '../../services/apis.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  constructor(private snack:MatSnackBar, private service:ApisService,private router:Router) { }
+  public signupData={
+    username:'',
+    email:'',
+    password:'',
+    fullname:'',
+    phNo:'',
+    age:'',
+    city:'',
+  }
+
+  public signup(){
+    console.log(this.signupData);
+    if(false){
+      this.snack.open('All fields are required','',{
+        duration:3000
+      });
+      
+    }else{
+      this.service.signup(this.signupData).subscribe((data:any)=>{
+        console.log(data);
+        this.snack.open('Signup Successfull','',{
+          duration:3000
+        });
+        this.router.navigateByUrl('/login');
+      },(error)=>{
+        console.log(error);
+        this.snack.open('Something went wrong','',{
+          duration:3000
+        });
+      });
+    }
+    
+
+    
+  }
 
 }
--- WritingDeskFront/src/app/services/apis.service.ts
+++ WritingDeskFront/src/app/services/apis.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baseUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,13 @@ import { Injectable } from '@angular/core';
 export class ApisService {
 
   constructor(private http:HttpClient) { }
+
+
+  public login(username:string,password:string){
+    return this.http.get(`${baseUrl}/login/${username}/${password}`);
+  }
+
+  public signup(user:any){
+    return this.http.post(`${baseUrl}/signup`,user);
+  }
 }
