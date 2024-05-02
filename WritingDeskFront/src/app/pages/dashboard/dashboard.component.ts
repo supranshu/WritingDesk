4c143b91d5f971322fc76cda6328cadc7896ee22
SUPRANSHU SINGH <ss445198@gmail.com>
2024-05-02 15:09:35 +0530

    FINAL PUSH1

--- WritingDeskFront/src/app/pages/dashboard/dashboard.component.ts
+++ WritingDeskFront/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,3 @@
-// Dashboard Component TypeScript
 import { Component, OnInit } from '@angular/core';
 import { ApisService } from '../../services/apis.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,29 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  posts: any[] = []; // Array to hold the list of posts
+  posts: any[] = [];
+  filteredPosts: any[] = [];
+  filterUsername: string = '';
 
   constructor(private service: ApisService, private snack: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
     if (!localStorage.length) {
-      
       this.router.navigateByUrl('/login');
     }
     this.loadAllPosts();
-    
   }
 
   loadAllPosts(): void {
     this.service.getAllPosts().subscribe(
       (data: any) => {
-        this.posts = data; // Assign the retrieved posts to the posts array
+        this.posts = data;
+        this.filteredPosts = data; // Initialize filteredPosts with all posts
       },
       (error) => {
         console.log(error);
-        this.snack.open('Something went wrong', '', {
-          duration: 3000
-        });
+        this.snack.open('Something went wrong', '', { duration: 3000 });
       }
     );
   }
@@ -41,5 +39,19 @@ export class DashboardComponent implements OnInit {
     localStorage.setItem('contactedUser', username);
     this.router.navigateByUrl('/contact');
   }
-  
+
+  filterByUsername(): void {
+    if (this.filterUsername.trim() === '') {
+      this.filteredPosts = this.posts; // Show all posts if the filter is empty
+    } else {
+      this.filteredPosts = this.posts.filter(post =>
+        post.username.toLowerCase().includes(this.filterUsername.toLowerCase())
+      );
+    }
+  }
+  showFilter: boolean = false;
+
+toggleFilter(): void {
+  this.showFilter = !this.showFilter;
+}
 }
